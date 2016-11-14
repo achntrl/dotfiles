11cc9dfaa079155c861f79d3e85803c4a8840e55
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2016-11-14 17:19:35 +0100

    Change to the sdsync operation (now a script)

--- misc/sdsync.sh
+++ misc/sdsync.sh
@@ -1,11 +1,14 @@
 #! /usr/local/bin/zsh 
 
+# Rsync folders between the mac and an SD card to have backups
 function sdsync() {
 	echo "################################################################################";
 	date;
 	rsync -ah --delete --stats --ignore-errors --force /Users/alexandre/Drive /Volumes/MacSD/;
 	echo '\n';
 	rsync -ah --delete --stats --ignore-errors --force /Users/alexandre/Documents /Volumes/MacSD/;
+	echo '\n';
+	rsync -ah --delete --stats --ignore-errors --force /Users/alexandre/Pictures /Volumes/MacSD/;
 	echo '\n\n';
 
 }
